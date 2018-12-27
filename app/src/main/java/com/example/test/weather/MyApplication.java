0dd56bd5cee0edc2d5580acd51b1f413918cb0f2
zsp <330963138@qq.com>
2018-12-27 17:25:04 +0800

    选择城市（2）

---
+++ app/src/main/java/com/example/test/weather/MyApplication.java
@@ -0,0 +1,96 @@
+package com.example.test.weather;
+
+import android.app.Application;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyApplication extends Application {
+    private static Application mApp;
+    List<City> cityList;
+    CityDB mCityDB;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d("MyApplication","OnCreate");
+        mApp = this;
+
+        mCityDB = openCityDB();
+        initCityList();
+    }
+
+    public static Application getInstance()
+    {
+        return mApp;
+    }
+
+    public CityDB openCityDB()
+    {
+        String path = "/data"
+                + Environment.getDataDirectory().getAbsolutePath()
+                + File.separator + getPackageName()
+//                + File.separator + "databases"
+                + File.separator + CityDB.CITY_DB_NAME;
+        Log.d("file path",path);
+        File db = new File(path);
+        Log.d("db",path);
+
+        try {
+            InputStream is = getAssets().open("city.db");
+
+            FileOutputStream fos = new FileOutputStream(db);
+            int len = -1;
+            byte[] buffer = new byte[1024];
+            while((len = is.read(buffer))!=-1)
+            {
+                fos.write(buffer,0,len);
+                //Log.d("write buffer",buffer.toString());
+                fos.flush();
+            }
+            fos.close();
+            is.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        return new CityDB(this,path);
+    }
+
+    private boolean prepareCityList()
+    {
+        cityList = mCityDB.getCityList();
+        for(City city:cityList)
+        {
+            String cityName = city.getCity();
+            Log.d("CityDB",cityName);
+        }
+        return true;
+    }
+
+    private void initCityList()
+    {
+        //CityList = new ArrayList<City>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                prepareCityList();
+            }
+        }).start();
+    }
+
+    private List<City> getCityList()
+    {
+        return cityList;
+    }
+
+}
+
