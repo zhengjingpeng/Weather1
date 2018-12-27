28c5944243dcbc4e24deb6a74ef2e5b1560be0d6
1 <2358387317@qq.com>
2018-12-27 15:06:22 +0800

    nadaoshuju

--- app/src/main/java/com/example/test/weather/MainActivity.java
+++ app/src/main/java/com/example/test/weather/MainActivity.java
@@ -2,12 +2,73 @@ package com.example.test.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private ImageView UpdatBtn;
+private TextView cityNameT,cityT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UpdatBtn=findViewById(R.id.title_city_update);
+        UpdatBtn.setOnClickListener(this);
+
+        //网络状态
+        if(CheckNet.getNetState(this)==CheckNet.NET_NONE){
+            Log.d("net","网络不通");
+        }else
+        {
+            Log.d("net","网络通畅");
+            getWeatherDatafromNet("101010100");
+        }
+    }
+    private void getWeatherDatafromNet(String cityCode) {
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey="+cityCode;
+        Log.d("Address:",address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection urlConnection = null;
+                try {
+                    URL url = new URL(address);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setConnectTimeout(8000);
+                    urlConnection.setReadTimeout(8000);
+                    InputStream in = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuffer sb = new StringBuffer();
+                    String str;
+                    while((str=reader.readLine())!=null)
+                    {
+                        sb.append(str);
+                        Log.d("date from url",str);
+                    }
+                    String response = sb.toString();
+                    Log.d("response",response);
+                }catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.title_city_update){
+            getWeatherDatafromNet("101010100");
+        }
     }
 }
